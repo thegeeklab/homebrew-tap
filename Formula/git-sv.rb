c71773f5fcbf2243f447f5449b02a0d1776bedc8
pat-s <patrick.schratz@gmail.com>
2024-10-24 23:37:45 +0200

    feat: add git-sv formula

---
+++ Formula/git-sv.rb
@@ -0,0 +1,31 @@
+class GitSv < Formula
+  desc "Semantic versioning tool for git based on conventional commits"
+  homepage "https://github.com/thegeeklab/git-sv"
+  url "https://github.com/thegeeklab/git-sv/archive/refs/tags/v1.0.5.tar.gz"
+  sha256 "2688f40faa50517f8ed41a3deac93cd55f6cdd6974a3665f497ff8840bcf36d9"
+  license "MIT"
+
+  head do
+    url "https://github.com/thegeeklab/git-sv.git", branch: "main"
+  end
+
+  livecheck do
+    url :stable
+    strategy :github_latest
+  end
+
+  depends_on "go" => :build
+
+  def install
+    build_date = Time.now.utc.strftime("%Y-%m-%dT%H:%M:%SZ")
+    ldflags = "-s -w -X main.BuildVersion=v#{version} -X main.BuildDate=#{build_date}"
+    system "go", "build", *std_go_args(ldflags:), "./cmd/git-sv"
+  end
+
+  test do
+    assert_match "git-sv - Semantic version for git", shell_output("#{bin}/git-sv --help")
+
+    # Additional test to check version command
+    assert_match "v1.0.5", shell_output("#{bin}/git-sv --version")
+  end
+end
